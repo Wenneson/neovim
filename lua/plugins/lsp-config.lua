a25d54759712fa418dab1efa9220ceb6dd996cfc
Wenneson <michael.l.wenneson@gmail.com>
2024-05-04 17:05:44 -0500

    update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,6 +23,7 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
+
       lspconfig.tsserver.setup({
         capabilities = capabilities
       })
@@ -35,8 +36,10 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      lspconfig.markdown_oxide.setup({
+        capabilities = capabilities
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
